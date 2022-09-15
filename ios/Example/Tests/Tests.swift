ceacb92284405c05f6a37a385a5dbcbd94e7706e
Cameron Hotchkies <chotchkies@squareup.com>
2022-09-15 16:18:10 -0700

    Replaced Quick/Nimble with xctest

--- ios/Example/Tests/Tests.swift
+++ ios/Example/Tests/Tests.swift
@@ -1,56 +1,11 @@
-// https://github.com/Quick/Quick
 
-import Quick
-import Nimble
 import CashSecuritySdk
+import XCTest
 
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-//        describe("these will fail") {
-//
-//            it("can do maths") {
-//                expect(1) == 2
-//            }
-//
-//            it("can read") {
-//                expect("number") == "string"
-//            }
-//
-//            it("will eventually fail") {
-//                expect("time").toEventually( equal("done") )
-//            }
-//            
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
-//        }
-        
-        describe("This test should pass") {
-            it("passes") {
-                expect(3) == 3
-            }
-        }
+class TableOfContentsSpec: XCTestCase {
+    
+    func test() {
+        XCTAssertTrue(true)
     }
+    
 }
