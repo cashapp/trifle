e6b6a94aaf10a2796c9d9711f643ed46b8a070cd
Dan Cashman <dcashman@squareup.com>
2022-10-26 14:10:18 -0700

    Remove default Library file and replace with Version interface impl.

---
+++ jvm/common/src/main/kotlin/app/cash/s2dk/common/LibraryVersion.kt
@@ -0,0 +1,13 @@
+package app.cash.s2dk.common
+
+class LibraryVersion : Version {
+
+  // TODO(dcashman): Get this value from the gradle build setup.
+  private val recordedVersion: String = "0.1.0"
+
+  override fun complete(): String = recordedVersion
+
+  override fun major(): Int = recordedVersion.split('.').firstOrNull()?.toInt() ?: -1
+
+  override fun minor(): Int = recordedVersion.split('.').elementAtOrNull(1)?.toInt() ?: -1
+}
