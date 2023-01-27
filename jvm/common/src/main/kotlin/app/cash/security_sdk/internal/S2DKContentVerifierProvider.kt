c719b91ed87de8e4fe23196f4a5096232ddd0306
Dan Cashman <dcashman@squareup.com>
2023-01-26 17:18:24 -0800

    Port: "Root cert gen" (#18)
    Add createRootSigningCertificate() to CertificateUtil.
    
    This method associates an entity name, validity period and Tink Keyset together into a
    self-signed certificate which can be used as the root of trust for a certificate chain.

---
+++ jvm/common/src/main/kotlin/app/cash/security_sdk/internal/S2DKContentVerifierProvider.kt
@@ -0,0 +1,47 @@
+package app.cash.security_sdk.internal
+
+import app.cash.security_sdk.SigningCert
+import com.google.crypto.tink.BinaryKeysetReader
+import com.google.crypto.tink.CleartextKeysetHandle
+import com.google.crypto.tink.PublicKeyVerify
+import org.bouncycastle.asn1.ASN1ObjectIdentifier
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier
+import org.bouncycastle.cert.X509CertificateHolder
+import org.bouncycastle.operator.ContentVerifier
+import org.bouncycastle.operator.ContentVerifierProvider
+
+/**
+ * Internal s2dk class to enable bouncycastle to delegate signing to Tink primitives rather than
+ * requiring Tink key users to extract the material. This should enable s2dk Clients to supply Tink
+ * keys directly without needing to reason about their internals.
+ */
+internal class S2DKContentVerifierProvider(
+  private val signingCert: SigningCert
+) : ContentVerifierProvider {
+  private val x509Certificate = X509CertificateHolder(signingCert.certificate.toByteArray())
+
+  override fun get(algorithmIdentifer: AlgorithmIdentifier): ContentVerifier {
+    val subjectPublicKeyInfo = x509Certificate.subjectPublicKeyInfo
+
+    // TODO(dcashman): this is effectively ignored at the moment, but we should ensure that the
+    // the algorithm passed-in matches our expected Tink algorithm ID.
+    if (!algorithmIdentifer.equals(AlgorithmIdentifier(ASN1ObjectIdentifier(TinkContentSigner.ED25519_OID)))
+      || !algorithmIdentifer.equals(subjectPublicKeyInfo.algorithm)) {
+      throw UnsupportedOperationException("Unknown/unsupported AlgorithmId provided to obtain S2DK ContentVerifier")
+    }
+
+    // Create PublicKeyVerify instance for verifier.
+    return TinkContentVerifier(
+      CleartextKeysetHandle.read(BinaryKeysetReader.withBytes(subjectPublicKeyInfo.publicKeyData.bytes))
+        .getPrimitive(PublicKeyVerify::class.java)
+    )
+  }
+
+  override fun getAssociatedCertificate(): X509CertificateHolder {
+    return x509Certificate
+  }
+
+  override fun hasAssociatedCertificate(): Boolean {
+    return true
+  }
+}
---
+++ jvm/common/src/test/kotlin/app/cash/security_sdk/CertificateUtilTest.kt
@@ -0,0 +1,81 @@
+package app.cash.security_sdk
+
+import app.cash.security_sdk.internal.S2DKContentVerifierProvider
+import com.google.crypto.tink.KeyTemplates
+import com.google.crypto.tink.KeysetHandle
+import com.google.crypto.tink.signature.SignatureConfig
+import org.bouncycastle.cert.X509CertificateHolder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.Duration
+import java.time.Period
+
+internal class CertificateUtilTest {
+  private lateinit var ed25519PrivateKeysetHandle: KeysetHandle
+
+  @BeforeEach
+  fun setUp() {
+    SignatureConfig.register()
+    ed25519PrivateKeysetHandle = KeysetHandle.generateNew(KeyTemplates.get("ED25519"))
+  }
+
+  @Test
+  fun `test createRootSigningCertificate validity period`() {
+    val cert = CertificateUtil.createRootSigningCertificate(
+      "entity", Period.ofDays(1), ed25519PrivateKeysetHandle
+    )
+
+    val certHolder = X509CertificateHolder(cert.certificate.toByteArray())
+    val duration =
+      Duration.ofMillis(
+        certHolder.notAfter.toInstant().minusMillis(certHolder.notBefore.toInstant().toEpochMilli())
+          .toEpochMilli()
+      )
+    assertEquals(1, duration.toDays())
+  }
+
+  @Test
+  fun `test createRootSigningCertificate certificate entity`() {
+    val cert = CertificateUtil.createRootSigningCertificate(
+      "entity", Period.ofDays(1), ed25519PrivateKeysetHandle
+    )
+
+    // Extract cert and then entity value.
+    val certHolder = X509CertificateHolder(cert.certificate.toByteArray())
+    assertEquals("CN=entity", certHolder.issuer.toString())
+  }
+
+  @Test
+  fun `test createRootSigningCertificate certificate issuer`() {
+    val cert = CertificateUtil.createRootSigningCertificate(
+      "entity", Period.ofDays(1), ed25519PrivateKeysetHandle
+    )
+
+    // Extract cert and then entity value.
+    val certHolder = X509CertificateHolder(cert.certificate.toByteArray())
+    assertEquals("CN=entity", certHolder.subject.toString())
+  }
+
+  @Test
+  fun `test createRootSigningCertificate signing`() {
+    // Create root signing cert using our keyset.  Root cert is self-signed.
+    val signingCert = CertificateUtil.createRootSigningCertificate(
+      "entity", Period.ofDays(1), ed25519PrivateKeysetHandle
+    )
+
+    // Extract the x.509 certificate from our object.
+    val certHolder = X509CertificateHolder(signingCert.certificate.toByteArray())
+
+    // Self-signed cert should verify when presented with itself.
+    assertTrue(certHolder.isSignatureValid(S2DKContentVerifierProvider(signingCert)))
+
+    // Different key should not verify
+    val otherCert = CertificateUtil.createRootSigningCertificate(
+      "entity", Period.ofDays(1), KeysetHandle.generateNew(KeyTemplates.get("ED25519"))
+    )
+    assertFalse(certHolder.isSignatureValid(S2DKContentVerifierProvider(otherCert)))
+  }
+}
