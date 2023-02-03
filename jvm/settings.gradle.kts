fd09fb85b76bc2af010d8f41520106aa8c9691dc
Dan Cashman <dcashman@squareup.com>
2023-02-03 15:02:38 -0800

    Publish Build Scans from GitHub Actions
    See reference documentation at:
    https://docs.gradle.org/current/userguide/github-actions.html#introduction

--- jvm/settings.gradle.kts
+++ jvm/settings.gradle.kts
@@ -8,4 +8,18 @@
  */
 
 rootProject.name = "jvm"
-include(":common")
+include("common")
+
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
