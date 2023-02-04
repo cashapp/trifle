fdcc3615084c738e2f45341539f547ea4110e1bd
Alvin See <alvin@squareup.com>
2023-02-03 16:03:38 -0800

    Implement a secure enclave wrapped keymanager for digital signature

---
+++ ios/SecuritySdk/Classes/Digital Signature/DigitalSignatureKeyManager.swift
@@ -0,0 +1,28 @@
+//
+//  DigitalSignatureKeyManager.swift
+//  SecuritySdk
+//
+
+import Foundation
+
+/// Key manager for digital signature keypairs
+protocol DigitalSignatureKeyManager {
+    associatedtype PrivateKey
+    associatedtype PublicKey
+
+    /**
+     Fetches the signing key
+     
+     - throws: an error type `CryptographicKeyError`
+     - returns: the private key for signing
+     */
+    func signingKey() throws -> PrivateKey
+
+    /**
+     Fetches the verifying key
+     
+     - throws: an error type `CryptographicKeyError`
+     - returns: the public key for verifying
+     */
+    func verifyingKey() throws -> PublicKey
+}
---
+++ ios/SecuritySdk/Classes/Digital Signature/Secure Enclave/SecureEnclaveVerifyingKey.swift
@@ -0,0 +1,40 @@
+//
+//  SecureEnclaveVerifyingKey.swift
+//  SecuritySdk
+//
+
+import Foundation
+
+public struct SecureEnclaveVerifyingKey: VerifyingKey {
+
+    // MARK: - Internal Properties
+
+    internal static let algorithm: SecKeyAlgorithm = .ecdsaSignatureMessageX962SHA256
+    internal let publicKey: SecKey
+
+    // MARK: - Initialization
+
+    init(secKey: SecKey) throws {
+        guard SecKeyIsAlgorithmSupported(secKey, .verify, Self.algorithm) else {
+            throw CryptographicKeyError.unsupportedAlgorithm
+        }
+        self.publicKey = secKey
+    }
+
+    // MARK: - Public Class Methods (VerifyingKey)
+
+    public func verify(data: Data, with signature: Data) -> Bool {
+        var error: Unmanaged<CFError>?
+        guard SecKeyVerifySignature(
+            self.publicKey,
+            Self.algorithm,
+            data as CFData,
+            signature as CFData,
+            &error
+        ) else {
+            return false
+        }
+
+        return true
+    }
+}
