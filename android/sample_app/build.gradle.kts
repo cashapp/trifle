c2a2769f72784cf12d9d1beaf372e1e283878a36
Jesse Wilson <jwilson@squareup.com>
2023-02-06 13:26:10 -0500

    Combine the two Gradle projects into one
    Signed-off-by: Jesse Wilson <jwilson@squareup.com>

--- android/sample_app/build.gradle.kts
+++ android/sample_app/build.gradle.kts
@@ -27,18 +27,14 @@ android {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
   }
-  kotlinOptions {
-    jvmTarget = "11"
-  }
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
   implementation("androidx.core:core-ktx:1.3.1")
   implementation("androidx.appcompat:appcompat:1.2.0")
   implementation("com.google.android.material:material:1.2.1")
   implementation("androidx.constraintlayout:constraintlayout:2.0.1")
-  implementation(project(":cash-security-s2dk"))
+  implementation(project(":android:cash-security-s2dk"))
   testImplementation("junit:junit:4.+")
   androidTestImplementation("androidx.test.ext:junit:1.1.2")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
--- android/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "Cash Security Sdk"
-include(":sample_app")
-include(":cash-security-s2dk")
-includeBuild("../jvm")
-
