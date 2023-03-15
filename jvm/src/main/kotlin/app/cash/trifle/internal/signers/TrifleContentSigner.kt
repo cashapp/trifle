4d28b769a3691bdb90d80441f791e522f65b43a4
Dan Cashman <dcashman@squareup.com>
2023-03-15 16:43:10 -0700

    Add CertRequest tests.

--- jvm/src/main/kotlin/app/cash/trifle/internal/signers/TrifleContentSigner.kt
+++ jvm/src/main/kotlin/app/cash/trifle/internal/signers/TrifleContentSigner.kt
@@ -1,8 +1,9 @@
 package app.cash.trifle.internal.signers
 
+import com.google.common.annotations.VisibleForTesting
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
 import org.bouncycastle.operator.ContentSigner
 
-internal sealed class TrifleContentSigner : ContentSigner {
+internal abstract class TrifleContentSigner : ContentSigner {
   abstract fun subjectPublicKeyInfo(): SubjectPublicKeyInfo
 }
