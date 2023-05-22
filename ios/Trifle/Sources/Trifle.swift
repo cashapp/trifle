e880f0f2714edd31985581cfb8043ba0b1bfd1ea
Gelareh Taban <gelareh@squareup.com>
2023-05-22 13:30:02 -0700

    bump up minor version of iOS lib (#115)

--- ios/Trifle/Sources/Trifle.swift
+++ ios/Trifle/Sources/Trifle.swift
@@ -8,7 +8,7 @@ import Wire
 
 public class Trifle {
     
-    public static let version = "0.1.5"
+    public static let version = "0.2.0"
     
     private static let mobileCertificateRequestVersion = UInt32(0)
     
