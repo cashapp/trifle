49293be872fde074ef6372bba46e5ebbb8018595
Gelareh Taban <gelareh@squareup.com>
2023-05-23 14:08:34 -0700

    bump up iOS version to 0.2.1 (#123)

--- ios/Trifle/Sources/Trifle.swift
+++ ios/Trifle/Sources/Trifle.swift
@@ -8,7 +8,7 @@ import Wire
 
 public class Trifle {
     
-    public static let version = "0.2.0"
+    public static let version = "0.2.1"
     
     private static let mobileCertificateRequestVersion = UInt32(0)
     
