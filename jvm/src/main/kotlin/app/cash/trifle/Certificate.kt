21907e219eef615aa602cd9872cf2d78ed3877ea
Dan Cashman <dcashman@squareup.com>
2023-06-02 10:57:50 -0700

    Add verify() method to the jvm certificate representation.
    Check that the provided certificate matches the associated certificate
    chain and also that the certificate contains the same information as the
    passed-in certificate request.

--- jvm/src/main/kotlin/app/cash/trifle/Certificate.kt
+++ jvm/src/main/kotlin/app/cash/trifle/Certificate.kt
@@ -1,6 +1,11 @@
 package app.cash.trifle
 
+import app.cash.trifle.CertificateRequest.PKCS10Request
+import app.cash.trifle.internal.validators.CertChainValidatorFactory
 import okio.ByteString.Companion.toByteString
+import org.bouncycastle.cert.X509CertificateHolder
+import org.bouncycastle.cert.X509v3CertificateBuilder
+import java.security.cert.X509Certificate
 import app.cash.trifle.protos.api.alpha.Certificate as CertificateProto
 
 /**
@@ -21,6 +26,40 @@ data class Certificate internal constructor(
       certificate = certificate.toByteString(),
       version = CERTIFICATE_VERSION
     ).encode()
+
+  /**
+   * Verify that the provided certificate matches what we expected.
+   * It matches the CSR that we have and the root cert is what
+   * we expect.
+   *
+   * @param certificateRequest -  request used to generate this certificate
+   * @param certificateChain - list of certificates with each subsequent certificate signing the one
+   *   before it, starting with the parent of this certificate.
+   * @return - false if certificate is not signed by the given root certificate, or if the
+   *   information present doesn't match that of the certificateRequest.
+   *   //TODO(dcashman): Return errors specific to each type of failure so clients can remediate.
+   */
+  fun verify(
+    certificateRequest: CertificateRequest,
+    certificateChain: List<Certificate>,
+    rootCertificate: Certificate
+  ): Boolean {
+    // First check to see if the certificate chain matches
+    val validator = CertChainValidatorFactory.get(rootCertificate)
+    if (!validator.validate(listOf(this) + certificateChain)) return false
+
+    // Certificate chain matches, check with certificate request.
+    // TODO(dcashman): Check other attributes as well.
+    val x509Certificate = X509CertificateHolder(certificate)
+    return when (certificateRequest) {
+      is PKCS10Request -> {
+        certificateRequest.pkcs10Req.subject.equals(x509Certificate.subject) && certificateRequest.pkcs10Req.subjectPublicKeyInfo.equals(
+          x509Certificate.subjectPublicKeyInfo
+        )
+      }
+      else -> false
+    }
+  }
 
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
