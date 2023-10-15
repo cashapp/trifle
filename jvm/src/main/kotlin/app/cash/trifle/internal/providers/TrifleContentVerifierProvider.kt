892897a10e9c7cf1858ed9c7d10afeb781f5034c
Alvin See <alvin@squareup.com>
2023-10-15 15:19:57 -0700

    Re-define TrifleContentVerifierProvider as sealed interface

--- jvm/src/main/kotlin/app/cash/trifle/internal/providers/TrifleContentVerifierProvider.kt
+++ jvm/src/main/kotlin/app/cash/trifle/internal/providers/TrifleContentVerifierProvider.kt
@@ -2,4 +2,4 @@ package app.cash.trifle.internal.providers
 
 import org.bouncycastle.operator.ContentVerifierProvider
 
-internal sealed class TrifleContentVerifierProvider : ContentVerifierProvider
+internal sealed interface TrifleContentVerifierProvider : ContentVerifierProvider
