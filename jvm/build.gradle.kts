0402240eac0ca10a51fdb42e071f2c03ed4f02cf
Alvin See <alvin@squareup.com>
2023-10-15 16:57:15 -0700

    Pull tink implementation into a separate module

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -1,28 +1,27 @@
 plugins {
-  kotlin("jvm")
-  id("org.jetbrains.dokka")
-  id("com.squareup.wire")
-  id("com.vanniktech.maven.publish.base")
+    kotlin("jvm")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish.base")
 }
 
 repositories {
-  // Use Maven Central for resolving dependencies.
-  mavenCentral()
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
 }
 
 tasks.jar {
-  manifest {
-    attributes(
-      mapOf(
-        "Implementation-Title" to project.name,
-        "Implementation-Version" to project.version
-      )
-    )
-  }
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
 }
 
 tasks.test {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 apply(plugin = "kotlin")
@@ -33,40 +32,30 @@ repositories {
 }
 
 dependencies {
-  implementation(libs.bcProv)
-  implementation(libs.bcPkix)
-  implementation(libs.tink)
-  implementation(libs.wire)
-  testImplementation(libs.junitApi)
-  testImplementation(libs.junitEngine)
+    api(project(":common"))
 
-  testImplementation(project(":jvm-testing"))
+    implementation(libs.bcProv)
+    implementation(libs.bcPkix)
+    implementation(libs.tink)
+
+    testImplementation(libs.junitApi)
+    testImplementation(libs.junitEngine)
+    testImplementation(project(":jvm-testing"))
 }
 
 // Manually add .proto files to the .jar.
 sourceSets {
-  main {
-    resources {
-      srcDir("../proto")
+    main {
+        resources {
+            srcDir("../proto")
+        }
     }
-  }
-}
-
-wire {
-  sourcePath {
-    srcDir(protosSrc)
-  }
-  protoPath {
-  }
-  kotlin {
-    javaInterop = true
-  }
 }
 
 configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
-  configure(
-    com.vanniktech.maven.publish.KotlinJvm(
-      javadocJar = com.vanniktech.maven.publish.JavadocJar.Dokka("dokkaHtml")
+    configure(
+        com.vanniktech.maven.publish.KotlinJvm(
+            javadocJar = com.vanniktech.maven.publish.JavadocJar.Dokka("dokkaHtml")
+        )
     )
-  )
 }
