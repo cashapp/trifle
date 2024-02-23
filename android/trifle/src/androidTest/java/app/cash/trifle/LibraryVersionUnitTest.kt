0eaadff858914920284d223250773f9eb285b821
Bruno Freitas <brunof@squareup.com>
2024-02-23 11:12:51 -0800

    Android release 0.2.10

--- android/trifle/src/androidTest/java/app/cash/trifle/LibraryVersionUnitTest.kt
+++ android/trifle/src/androidTest/java/app/cash/trifle/LibraryVersionUnitTest.kt
@@ -9,7 +9,7 @@ internal class LibraryVersionUnitTest {
     val libraryVersion = LibraryVersion()
     val versionString = libraryVersion.complete()
 
-    assertEquals(versionString, "0.2.9")
+    assertEquals(versionString, "0.2.10")
   }
 
   @Test
